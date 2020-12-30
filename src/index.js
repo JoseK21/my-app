1b61943a40ad06d3923bbf80006ff5f08b41664d
Jose Nunez <jose.nunez@advisiondevelopment.com>
2020-12-29 23:10:01 -0600

    width

--- src/index.js
+++ src/index.js
@@ -5,17 +5,19 @@ import { ReactSvgPanZoomLoader } from "react-svg-pan-zoom-loader";
 
 import "./index.css";
 
+const w = window.innerWidth;
+
 function App() {
   return (
     <div className="App">
       <h1>React SVG Pan Zoom Remote file loader</h1>
-      <h3>A React component that adds pan and zoom features to SVG</h3>
+      <h3>Width : ${w}</h3>
 
       <ReactSvgPanZoomLoader
         src="test-digram.svg"
         render={content => (
-          <UncontrolledReactSVGPanZoom width={500} height={500}>
-            <svg width={617} height={316}>
+          <UncontrolledReactSVGPanZoom width={w} height={500}>
+            <svg width={w} height={500}>
               <g>
                 <rect x="400" y="40" width="100" height="200" fill="#4286f4" stroke="#f4f142" onClick={() => alert('blue')} />
                 <circle cx="108" cy="108.5" r="100" fill="#0ff" stroke="#0ff" onClick={() => alert('cyan')} />
