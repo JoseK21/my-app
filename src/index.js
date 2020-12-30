c62c67a8dbb7cdba2213b8408ecad553cd734e50
Jose Nunez <jose.nunez@advisiondevelopment.com>
2020-12-29 22:46:52 -0600

    log to alert

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@ function App() {
           <UncontrolledReactSVGPanZoom width={500} height={500}>
             <svg width={617} height={316}>
               <g>
-                <rect x="400" y="40" width="100" height="200" fill="#4286f4" stroke="#f4f142" onClick={() => console.log('blue')} />
-                <circle cx="108" cy="108.5" r="100" fill="#0ff" stroke="#0ff" onClick={() => console.log('cyan')} />
-                <circle cx="180" cy="209.5" r="100" fill="#ff0" stroke="#ff0" onClick={() => console.log('yellow')} />
-                <circle cx="220" cy="109.5" r="100" fill="#f0f" stroke="#f0f" onClick={() => console.log('pink')} />
+                <rect x="400" y="40" width="100" height="200" fill="#4286f4" stroke="#f4f142" onClick={() => alert('blue')} />
+                <circle cx="108" cy="108.5" r="100" fill="#0ff" stroke="#0ff" onClick={() => alert('cyan')} />
+                <circle cx="180" cy="209.5" r="100" fill="#ff0" stroke="#ff0" onClick={() => alert('yellow')} />
+                <circle cx="220" cy="109.5" r="100" fill="#f0f" stroke="#f0f" onClick={() => alert('pink')} />
               </g>
             </svg>
           </UncontrolledReactSVGPanZoom>
